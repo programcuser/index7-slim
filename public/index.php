7f0186fe2c973fa4135e78300b8dce794e739f4b
Mr Smith <krygrish@yandex.ru>
2022-01-17 14:12:54 +0300

    add user post form

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ use function Symfony\Component\String\s;
 
 
 $users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
-
+$usersFilePath = __DIR__ . '/../files/users/users.txt';
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -24,6 +24,8 @@ $app->get('/', function ($request, $response) {
     //return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
+    //print_r(file_get_contents('example.txt', FILE_USE_INCLUDE_PATH));
+    //print_r(__DIR__);
     return $response->write('Welcome to Hexlet!');
 });
 //$app->run();
@@ -41,17 +43,57 @@ $app->get('/', function ($request, $response) {
 //});
 
 //нужно использовать библиотеки Symfony\Component\String\s из Symfony, collect из Laravel
-$app->get('/users', function ($request, $response) use ($users) {
-    $term = $request->getQueryParam('term');
-    $paramsArr = $request->getQueryParams();
-    $newUsers = collect($users)->map(fn($us) => s($us));
+//$app->get('/users', function ($request, $response) use ($users) {
+//    $term = $request->getQueryParam('term');
+//    $paramsArr = $request->getQueryParams();
+//    $newUsers = collect($users)->map(fn($us) => s($us));
+//
+//    if (count($paramsArr) !== 0) {
+//        $newUsers = $newUsers->filter(fn($us) => $us->startsWith($term));
+//    }
+//    //print_r($newUsers->toArray());
+//    $params = ['users' => $newUsers->toArray(), 'term' => $term];
+//    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+//});
 
-    if (count($paramsArr) !== 0) {
-        $newUsers = $newUsers->filter(fn($us) => $us->startsWith($term));
-    }
-    //print_r($newUsers->toArray());
-    $params = ['users' => $newUsers->toArray(), 'term' => $term];
-    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+$app->get('/users', function ($request, $response) use ($usersFilePath) {
+    $usersText = file_get_contents($usersFilePath);
+    //print_r($usersText);
+    //$usersArr = explode("\n", $usersText);
+    $usersArr = json_decode($usersText, true);
+    //$usersCount = count($usersArr);
+    //array_splice($usersArr, $usersCount - 1);
+    //var_dump(count($usersArr));
+    //$usersArr = $usersArr ?? [];
+    /*$newUsers = array_map(function ($us) {
+        $usr = json_decode($us, true);
+        print_r($usr);
+        //return $us;
+        return "{$usr['name']} - {$usr['nickname']}";
+    }, $usersArr);*/
+
+    $params = ['users' => $usersArr];
+    return $this->get('renderer')->render($response, 'users/users.phtml', $params);
+});
+
+$app->get('/users/new', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'users/new.phtml');
+});
+
+$app->post('/users', function ($request, $response) use ($usersFilePath) {
+    $user = $request->getParsedBodyParam('user');
+    
+    $usersText = file_get_contents($usersFilePath);
+    $usersArr = json_decode($usersText);
+    $usersNum = count($usersArr);
+
+    $user['id'] = $usersNum + 1;
+    $usersArr[] = $user;
+    $userJson = json_encode($usersArr, JSON_PRETTY_PRINT);
+    file_put_contents($usersFilePath, $userJson);
+
+    return $response->withRedirect('/users', 302);
+    //return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
